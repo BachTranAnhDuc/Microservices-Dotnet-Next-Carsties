62c1d0bd5a8b103f5646f9de422ac6a62e2da817
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-07 21:16:20 +0700

    feat(database): setup mongo db and seed data

---
+++ src/SearchService/Data/DbInitializer.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+
+namespace SearchService.Models;
+
+public class DbInitializer
+{
+    public static async Task InitDb(WebApplication app) {
+
+        await DB.InitAsync("SearchDb", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("MongoDbConnection")));
+
+        await DB.Index<Item>()
+            .Key(x => x.Make, KeyType.Text)
+            .Key(x => x.Model, KeyType.Text)
+            .Key(x => x.Color, KeyType.Text)
+            .CreateAsync();
+        
+        var count = await DB.CountAsync<Item>();
+
+        if (count == 0) {
+            System.Console.WriteLine("No data - will attempt to seed");
+            var itemData = await File.ReadAllTextAsync("Data/auction.json");
+
+            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
+
+            var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
+
+            await DB.SaveAsync(items);
+        }
+    }
+}
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,17 +1,26 @@
+using MongoDB.Driver;
+using MongoDB.Entities;
+using SearchService.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+try
+{
+    await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    System.Console.WriteLine(e);
+}
 
 app.Run();
