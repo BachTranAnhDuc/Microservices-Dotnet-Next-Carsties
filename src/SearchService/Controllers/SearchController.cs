2c50bb90b8d970d20de1c8332c69bbfeb76ad730
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-07 21:40:25 +0700

    feat(search): paginating the results

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -9,17 +9,24 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItem(string searchTerm) {
-        var query = DB.Find<Item>();
+    public async Task<ActionResult<List<Item>>> SearchItem(string searchTerm, int pageNumber = 1, int pageSize = 4) {
+        var query = DB.PagedSearch<Item>();
 
         query.Sort(x => x.Ascending(a => a.Make));
 
         if (!string.IsNullOrEmpty(searchTerm)) {
             query.Match(Search.Full, searchTerm).SortByTextScore();
         }
+        
+        query.PageNumber(pageNumber);
+        query.PageSize(pageSize);
 
         var result = await query.ExecuteAsync();
 
-        return result;
+        return Ok(new {
+            results = result.Results,
+            pageCount = result.PageCount,
+            totalCount = result.TotalCount
+        });
     }
 }
