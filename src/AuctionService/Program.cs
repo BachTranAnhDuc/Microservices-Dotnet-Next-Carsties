29d53cdbed489204f8e69192a7cbcd04872ab64b
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-07 14:08:19 +0700

    feat(dtos): adding dtos and auto mapper

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AuctionDbContext>((opt) => {
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
