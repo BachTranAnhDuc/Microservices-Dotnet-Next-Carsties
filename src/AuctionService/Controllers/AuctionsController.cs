9be3a5125d2827a3dc6dc6e6c19c15c613561b64
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-07 15:13:09 +0700

    feat(CRUD): adding the create auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -1,5 +1,6 @@
 ﻿using AuctionService.Data;
 using AuctionService.DTOs;
+using AuctionService.Entities;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,5 +36,21 @@ public class AuctionsController : ControllerBase
         if (auction == null) return NotFound();
 
         return _mapper.Map<AuctionDto>(auction);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<AuctionDto>> CreateAuction(CreateAuctionDto auctionDto) {
+        var auction = _mapper.Map<Auction>(auctionDto);
+
+        auction.Seller = "test seller";
+        auction.Winner = "test winner";
+
+        _context.Auctions.Add(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+        
+        if (!result) return BadRequest("Could not save changes to the DB");
+
+        return CreatedAtAction(nameof(GetAutionById), new { auction.Id }, _mapper.Map<AuctionDto>(auction));
     }
 }
