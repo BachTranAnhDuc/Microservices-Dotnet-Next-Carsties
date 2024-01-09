0a836fee796ed3f3fad2a736fdd0751cc24c2e53
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-09 20:34:27 +0700

    feat(search): adding filtering and ordering

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MongoDB.Entities;
 using SearchService.Models;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Controllers;
 
@@ -9,17 +10,36 @@ namespace SearchService.Controllers;
 public class SearchController : ControllerBase
 {
     [HttpGet]
-    public async Task<ActionResult<List<Item>>> SearchItem(string searchTerm, int pageNumber = 1, int pageSize = 4) {
-        var query = DB.PagedSearch<Item>();
+    public async Task<ActionResult<List<Item>>> SearchItem([FromQuery]SearchParams searchParams) {
+        var query = DB.PagedSearch<Item, Item>();
 
-        query.Sort(x => x.Ascending(a => a.Make));
 
-        if (!string.IsNullOrEmpty(searchTerm)) {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+        if (!string.IsNullOrEmpty(searchParams.SearchTerm)) {
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+        }
+
+        query = searchParams.OrderBy switch {
+            "make" => query.Sort(x => x.Ascending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+
+        query = searchParams.FilterBy switch {
+            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller)) {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner)) {
+            query.Match(x => x.Winner == searchParams.Winner);
         }
         
-        query.PageNumber(pageNumber);
-        query.PageSize(pageSize);
+        query.PageNumber(searchParams.PageNumber);
+        query.PageSize(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 
