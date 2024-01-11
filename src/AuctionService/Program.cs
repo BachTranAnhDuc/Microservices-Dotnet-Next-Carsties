7bc49ec5d35545b2159a6f7a7e81ba04eb1193a8
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-11 11:10:17 +0700

    feat(rabbitmq): finished rabbitmq

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,9 @@
 using AuctionService.Data;
 using Microsoft.EntityFrameworkCore;
 using MassTransit;
+using AuctionService.Entities;
+using Contracts;
+using AuctionService.Consumers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +14,17 @@ builder.Services.AddDbContext<AuctionDbContext>((opt) => {
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit((x) => {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o => {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
     x.UsingRabbitMq((context, cfg) => {
         cfg.ConfigureEndpoints(context);
     });
--- src/SearchService/Consumers/AuctionCreatedConsumer.cs
+++ src/SearchService/Consumers/AuctionCreatedConsumer.cs
@@ -21,6 +21,8 @@ public class AuctionCreatedConsumer : IConsumer<AuctionCreated>
 
         var item = _mapper.Map<Item>(context.Message);
 
+        if (item.Model == "Foo") throw new ArgumentException("Cannot sell cars with name of Foo");
+
         await item.SaveAsync();
     }
 }
