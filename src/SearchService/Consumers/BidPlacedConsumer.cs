8e1eff060539631c7f71779ea49f906fdc502a02
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-12 10:51:46 +0700

    feat(gateway): finish gateway config

---
+++ src/SearchService/Consumers/BidPlacedConsumer.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumers;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
+{
+    public BidPlacedConsumer()
+    {
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        System.Console.WriteLine("---> Consuming bid placed");
+
+        var auction = await DB.Find<Item>().OneAsync(context.Message.AuctionId);
+
+        if (context.Message.BidStatus.Contains("Accepted") && context.Message.Amount > auction.CurrentHighBid) {
+            auction.CurrentHighBid = context.Message.Amount;
+            await auction.SaveAsync();
+        }
+    }
+}
