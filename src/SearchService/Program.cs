17df149952ba047eee7a765da3ab9d45deec7457
bachtrananhduc <bachtrananhduc@gmail.com>
2024-01-12 20:13:54 +0700

    feat(docker): finish configure docker

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -19,6 +19,16 @@ builder.Services.AddMassTransit((x) => {
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("search", false));
 
     x.UsingRabbitMq((context, cfg) => {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ReceiveEndpoint("search-auction-created", e => {
+            e.UseMessageRetry(r => r.Interval(5, 5));
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
